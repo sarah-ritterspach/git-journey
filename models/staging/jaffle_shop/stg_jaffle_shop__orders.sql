786836fd3ebc4b17c3b9bff6b8fdf4c57dddb9d2
sarah.ritterspach@bytecode.io <sarah.ritterspach@bytecode.io>
2026-09-22 19:04:44 +0000

    added data tests

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -10,9 +10,9 @@ renamed as (
 
     select
         id as order_id,
-        user_id,
+        user_id as customer_id,
         order_date,
-        status,
+        status as order_status,
         _etl_loaded_at
 
     from source
